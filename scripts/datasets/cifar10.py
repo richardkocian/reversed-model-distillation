c0f9f638b00dae81548191ba4fd3a00cd6867203
Richard Kocián <richardkocian4@gmail.com>
2025-03-20 20:39:05 +0100

    Revert "feat: Set seed for data loader, use deterministic algorithms"
    This reverts commit f8c384b6db17814236ba5fcd1fa8d230494fe85a.

--- scripts/datasets/cifar10.py
+++ scripts/datasets/cifar10.py
@@ -1,8 +1,5 @@
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-import torch
-import random
-import numpy as np
 import config
 
 
@@ -17,19 +14,10 @@ def get_cifar10_datasets(datasets_path):
 
     return train_dataset, test_dataset
 
-def seed_worker(worker_id):
-    worker_seed = torch.initial_seed() % 2 ** 32
-    np.random.seed(worker_seed)
-    random.seed(worker_seed)
-
 def get_cifar10_loaders(datasets_path, batch_size, num_workers):
     train_dataset, test_dataset = get_cifar10_datasets(datasets_path)
-
-    g = torch.Generator()
-    g.manual_seed(0)
-
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers,
-                              pin_memory=config.PIN_MEMORY, persistent_workers=config.PERSISTENT_WORKERS, worker_init_fn=seed_worker,generator=g)
+                              pin_memory=config.PIN_MEMORY, persistent_workers=config.PERSISTENT_WORKERS)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers,
-                             pin_memory=config.PIN_MEMORY, persistent_workers=config.PERSISTENT_WORKERS, worker_init_fn=seed_worker,generator=g)
+                             pin_memory=config.PIN_MEMORY, persistent_workers=config.PERSISTENT_WORKERS)
     return train_loader, test_loader
