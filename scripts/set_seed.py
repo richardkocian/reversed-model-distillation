df1382fe40e7c89c0c7377567465548daa753efd
richardkocian <richardkocian4@gmail.com>
2025-03-20 16:15:23 +0100

    feat: Also setting random.seed

--- scripts/set_seed.py
+++ scripts/set_seed.py
@@ -1,8 +1,10 @@
 import numpy as np
 import torch
+import random
 
 def set_seed(seed):
     np.random.seed(seed)
+    random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
