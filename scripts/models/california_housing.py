0c99517a856deb7d1eca0a7bb406c22ce29a37a2
Richard Kocián <richardkocian4@gmail.com>
2025-04-02 13:16:59 +0200

    feat: Edited large teacher california_housing dataset

--- scripts/models/california_housing.py
+++ scripts/models/california_housing.py
@@ -48,10 +48,10 @@ class TeacherModelMediumCALIFORNIA(nn.Module):
 class TeacherModelLargeCALIFORNIA(nn.Module):
     def __init__(self):
         super(TeacherModelLargeCALIFORNIA, self).__init__()
-        self.fc1 = nn.Linear(8, 16)
-        self.fc2 = nn.Linear(16, 32)
-        self.fc3 = nn.Linear(32, 64)
-        self.fc4 = nn.Linear(64, 1)
+        self.fc1 = nn.Linear(8, 32)
+        self.fc2 = nn.Linear(32, 64)
+        self.fc3 = nn.Linear(64, 32)
+        self.fc4 = nn.Linear(32, 1)
         self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
